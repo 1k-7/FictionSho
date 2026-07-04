4e521ea89b8d1bc2c709221049e755ee65fd5d44
clocks <doomsdayrs@gmail.com>
2026-07-04 11:40:46 -0400

    misc(BackupWorker): Split writeBackup job into separate function
    That reduces the nesting hell

--- android/src/main/java/app/shosetsu/android/backend/workers/onetime/BackupWorker.kt
+++ android/src/main/java/app/shosetsu/android/backend/workers/onetime/BackupWorker.kt
@@ -280,171 +280,187 @@ class BackupWorker(appContext: Context, params: WorkerParameters) : CoroutineWor
 				e.printStackTrace()
 				emptyMap()
 			}
-
-
 			true
 		}
+
 		System.gc() // please clean up
 
 		if (success) {
-			logI("Loading repositories required")
-			notify("Loading repositories required")
-			// All the repos required for backup
-			// Contains only the repos that are used
-			val repositoriesRequired =
-				extensionRepoRepository.loadRepositories()
-					.filter { repositoryEntity ->
-						extensions.any { extensionEntity ->
-							extensionEntity.repoID == repositoryEntity.id
-						}
-					}.map { (id, url, name) ->
-						BackupRepositoryEntity(id, url, name)
-					}
+			val result = writeBackup(extensions, novelsToChapters, backupSettings, categories)
 
-			val zippedBytes = gzip { gzip ->
-				logI("Creating backup entity")
-				notify("Creating backup entity")
-				val backup = FleshedBackupEntity(
-					repos = repositoriesRequired,
-					// Creates the trees
-					extensions = extensions.map { extensionEntity ->
-						BackupExtensionEntity(
-							extensionEntity.id,
-							extensionEntity.repoID,
-							novelsToChapters.filter { (novel, _) ->
-								novel.extensionID == extensionEntity.id
-							}.map { (novel, chapters) ->
-								val settings =
-									if (backupSettings)
-										novelSettingsRepository.get(novel.id!!)
-									else null
-
-								val bSettings = settings?.let {
-									BackupNovelSettingEntity(
-										it.sortType,
-										it.showOnlyReadingStatusOf,
-										it.showOnlyBookmarked,
-										it.showOnlyDownloaded,
-										it.showOnlyString,
-										it.reverseOrder,
-									)
-								} ?: BackupNovelSettingEntity()
-
-								val novelCategories =
-									novelCategoriesRepository.getNovelCategoriesFromNovel(
-										novel.id!!
-									)
-										.map { categories[it.categoryID]!!.order }
-
-								BackupNovelEntity(
-									url = novel.url,
-									bookmarked = novel.bookmarked,
-									loaded = novel.loaded,
-									name = novel.title,
-									imageURL = novel.imageURL,
-									description = novel.description,
-									language = novel.language,
-									genres = novel.genres,
-									authors = novel.authors,
-									artists = novel.artists,
-									tags = novel.tags,
-									status = novel.status,
-									chapters = chapters,
-									settings = bSettings,
-									categories = novelCategories,
-									pinned = novelPinRepository.isPinned(novel.id!!)
-								)
-							}
-						)
-					},
-					categories = categories.values.toList()
-				)
-
-				logI("Encoding to json")
-				notify("Encoding to json")
-				backupJSON.encodeToStream(backup, gzip)
+			if (result == null) {
+				// Call GC to clean up the bulky resources
+				System.gc()
+				delay(500)
+				backupRepository.updateProgress(BackupProgress.COMPLETE)
+				return Result.success()
+			} else {
+				// Something must have happened...
+				return result
 			}
-			System.gc() // please clean up
-
-			logI("Saving to file")
-			notify("Saving to file")
-			val backupEntity = BackupEntity(zippedBytes)
-
-			try {
-				fun missing(): Result {
-					logE("Failed to create document")
-					notify(R.string.export_backup_notification_missing_uri) {
-						setNotOngoing()
-						addAction(
-							actionBuilder(
-								Icons.Default.Settings,
-								getString(R.string.worker_backup_set_folder),
-								PendingIntent.getActivity(
-									applicationContext,
-									0,
-									openAppToSetBackupDirectory,
-									if (SDK_INT >= VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
-								)
-							).build()
-						)
-					}
-					backupRepository.updateProgress(BackupProgress.FAILURE)
-					return Result.failure()
-				}
-
-				val directoryUri = backupStorageLocation() ?: return missing()
-				// If the directory URI is not provided, we can
-				val docId =
-					DocumentsContractCompat.getTreeDocumentId(directoryUri) ?: return missing()
-				val parentDocumentUri =
-					DocumentsContractCompat.buildDocumentUriUsingTree(directoryUri, docId)
-						?: return missing()
-				val uri = DocumentsContractCompat.createDocument(
-					applicationContext.contentResolver,
-					parentDocumentUri,
-					"application/octet-stream",
-					backupEntity.fileName
-				) ?: return missing()
-				writeToUri(uri, backupEntity)
-			} catch (e: NullContentResolverException) {
-				logE("Failed to write to URI", e)
-				notify(R.string.worker_export_backup_null_resolver) {
-					setNotOngoing()
-					addReportErrorAction(applicationContext, defaultNotificationID, e)
-				}
-				backupRepository.updateProgress(BackupProgress.FAILURE)
-				return Result.failure()
-			} catch (e: FileNotFoundException) {
-				logE("URI is invalid file", e)
-				notify(R.string.worker_export_backup_file_missing) {
-					setNotOngoing()
-					addReportErrorAction(applicationContext, defaultNotificationID, e)
-				}
-				backupRepository.updateProgress(BackupProgress.FAILURE)
-				return Result.failure()
-			} catch (e: FilePermissionException) {
-				logE("Invalid permission to file", e)
-				notify(R.string.worker_export_backup_missing_perm) {
-					setNotOngoing()
-					addReportErrorAction(applicationContext, defaultNotificationID, e)
-				}
-				backupRepository.updateProgress(BackupProgress.FAILURE)
-				return Result.failure()
-			}
-
-			notify(R.string.worker_backup_complete) {
-				setOngoing(false)
-			}
-
-			// Call GC to clean up the bulky resources
-			System.gc()
-			delay(500)
-			backupRepository.updateProgress(BackupProgress.COMPLETE)
-			return Result.success()
 		}
 
 		backupRepository.updateProgress(BackupProgress.FAILURE)
 		return Result.failure()
+	}
+
+	@Throws(IOException::class)
+	private suspend fun writeBackup(
+		extensions: List<InstalledExtensionEntity>,
+		novelsToChapters: List<Pair<NovelEntity, List<BackupChapterEntity>>>,
+		backupSettings: Boolean,
+		categories: Map<Int, BackupCategoryEntity>
+	): Result? {
+		logI("Loading repositories required")
+		notify("Loading repositories required")
+		// All the repos required for backup
+		// Contains only the repos that are used
+		val repositoriesRequired =
+			extensionRepoRepository.loadRepositories()
+				.filter { repositoryEntity ->
+					extensions.any { extensionEntity ->
+						extensionEntity.repoID == repositoryEntity.id
+					}
+				}.map { (id, url, name) ->
+					BackupRepositoryEntity(id, url, name)
+				}
+
+		val zippedBytes = gzip { gzip ->
+			logI("Creating backup entity")
+			notify("Creating backup entity")
+			val backup = FleshedBackupEntity(
+				repos = repositoriesRequired,
+				// Creates the trees
+				extensions = extensions.map { extensionEntity ->
+					BackupExtensionEntity(
+						extensionEntity.id,
+						extensionEntity.repoID,
+						novelsToChapters.filter { (novel, _) ->
+							novel.extensionID == extensionEntity.id
+						}.map { (novel, chapters) ->
+							val settings =
+								if (backupSettings)
+									novelSettingsRepository.get(novel.id!!)
+								else null
+
+							val bSettings = settings?.let {
+								BackupNovelSettingEntity(
+									it.sortType,
+									it.showOnlyReadingStatusOf,
+									it.showOnlyBookmarked,
+									it.showOnlyDownloaded,
+									it.showOnlyString,
+									it.reverseOrder,
+								)
+							} ?: BackupNovelSettingEntity()
+
+							val novelCategories =
+								novelCategoriesRepository.getNovelCategoriesFromNovel(
+									novel.id!!
+								)
+									.map { categories[it.categoryID]!!.order }
+
+							BackupNovelEntity(
+								url = novel.url,
+								bookmarked = novel.bookmarked,
+								loaded = novel.loaded,
+								name = novel.title,
+								imageURL = novel.imageURL,
+								description = novel.description,
+								language = novel.language,
+								genres = novel.genres,
+								authors = novel.authors,
+								artists = novel.artists,
+								tags = novel.tags,
+								status = novel.status,
+								chapters = chapters,
+								settings = bSettings,
+								categories = novelCategories,
+								pinned = novelPinRepository.isPinned(novel.id!!)
+							)
+						}
+					)
+				},
+				categories = categories.values.toList()
+			)
+
+			logI("Encoding to json")
+			notify("Encoding to json")
+			backupJSON.encodeToStream(backup, gzip)
+		}
+		System.gc() // please clean up
+
+		logI("Saving to file")
+		notify("Saving to file")
+		val backupEntity = BackupEntity(zippedBytes)
+
+		try {
+			fun missing(): Result {
+				logE("Failed to create document")
+				notify(R.string.export_backup_notification_missing_uri) {
+					setNotOngoing()
+					addAction(
+						actionBuilder(
+							Icons.Default.Settings,
+							getString(R.string.worker_backup_set_folder),
+							PendingIntent.getActivity(
+								applicationContext,
+								0,
+								openAppToSetBackupDirectory,
+								if (SDK_INT >= VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+							)
+						).build()
+					)
+				}
+				backupRepository.updateProgress(BackupProgress.FAILURE)
+				return Result.failure()
+			}
+
+			val directoryUri = backupStorageLocation() ?: return missing()
+			// If the directory URI is not provided, we can
+			val docId =
+				DocumentsContractCompat.getTreeDocumentId(directoryUri) ?: return missing()
+			val parentDocumentUri =
+				DocumentsContractCompat.buildDocumentUriUsingTree(directoryUri, docId)
+					?: return missing()
+			val uri = DocumentsContractCompat.createDocument(
+				applicationContext.contentResolver,
+				parentDocumentUri,
+				"application/octet-stream",
+				backupEntity.fileName
+			) ?: return missing()
+			writeToUri(uri, backupEntity)
+		} catch (e: NullContentResolverException) {
+			logE("Failed to write to URI", e)
+			notify(R.string.worker_export_backup_null_resolver) {
+				setNotOngoing()
+				addReportErrorAction(applicationContext, defaultNotificationID, e)
+			}
+			backupRepository.updateProgress(BackupProgress.FAILURE)
+			return Result.failure()
+		} catch (e: FileNotFoundException) {
+			logE("URI is invalid file", e)
+			notify(R.string.worker_export_backup_file_missing) {
+				setNotOngoing()
+				addReportErrorAction(applicationContext, defaultNotificationID, e)
+			}
+			backupRepository.updateProgress(BackupProgress.FAILURE)
+			return Result.failure()
+		} catch (e: FilePermissionException) {
+			logE("Invalid permission to file", e)
+			notify(R.string.worker_export_backup_missing_perm) {
+				setNotOngoing()
+				addReportErrorAction(applicationContext, defaultNotificationID, e)
+			}
+			backupRepository.updateProgress(BackupProgress.FAILURE)
+			return Result.failure()
+		}
+
+		notify(R.string.worker_backup_complete) {
+			setOngoing(false)
+		}
+
+		return null
 	}
 
 	/**
